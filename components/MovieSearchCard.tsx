b78a37ea862724145cf8420f6a89725dfade8b5b
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-04 18:03:40 +0100

    Style top part of details

--- components/MovieSearchCard.tsx
+++ components/MovieSearchCard.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { getCredit } from "@/app/actions/getCredit";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Cast } from "@/types";
 
