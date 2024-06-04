e9fc760658c6404ce6c1ddea6afccfd1f2f1ce1a
Akin Akinbobola <akinbobola71@gmail.com>
2024-06-04 13:59:22 +0100

    Add tabs

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Page = () => {
-  return <div>Hello World</div>;
+  return (
+    <main className={"mt-10"}>
+      <div>
+        <h1 className={"text-yellow text-3xl"}>Featured Today</h1>
+
+        <Tabs defaultValue="movies" className="w-full mt-3">
+          <TabsList className={"w-full"}>
+            <TabsTrigger value="movies">Movies</TabsTrigger>
+            <TabsTrigger value="series">Series</TabsTrigger>
+          </TabsList>
+          <TabsContent value="movies" className={"text-white"}>
+            Make changes to your account here.
+          </TabsContent>
+          <TabsContent value="series" className={"text-white"}>
+            Change your password here.
+          </TabsContent>
+        </Tabs>
+      </div>
+    </main>
+  );
 };
 
 export default Page;
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header className={"flex items-center justify-between w-full gap-4 py-4"}>
       <Link href={"/"}>
-        <Image src={"/icons/logo.svg"} alt={"Logo"} width={80} height={80} />
+        <Image src={"/icons/logo.svg"} alt={"Logo"} width={100} height={100} />
       </Link>
 
       <SearchBar />
